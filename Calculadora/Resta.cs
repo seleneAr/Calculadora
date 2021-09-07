ae7081411a6f3bd37276def45e0a9468c43f849b
Selene <selene.ajtd@gmail.com>
2021-09-07 16:36:43 +0200

    Resta

---
+++ Calculadora/Resta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public class Resta
+    {
+
+        /// <summary>
+        /// Resta normal de numeros. Se necesitan minimo 2 numeros para hacer una resta.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static decimal Subtraction(decimal[] numbers)
+        {
+
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal resta = 0;
+
+            foreach (var number in numbers)
+            {
+                resta -= number;
+            }
+
+            return resta;
+        }
+
+
+        /// <summary>
+        /// Resta de dos numeros. El primero tiene que ser mayor que el segundo.
+        /// </summary>
+        public static decimal SubtractionTwoNumbers(decimal num1, decimal num2) {
+          
+            if(num1 < num2)
+            {
+                throw new ArgumentException();
+            }
+
+            return num1 - num2;
+        }
+
+
+
+    }
+}
