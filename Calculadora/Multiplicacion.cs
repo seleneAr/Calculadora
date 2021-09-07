f509e3532f2c88652982be0264216c69877ebc06
Selene <selene.ajtd@gmail.com>
2021-09-07 17:07:23 +0200

    Multiplicacion

--- Calculadora/Multiplicacion.cs
+++ Calculadora/Multiplicacion.cs
@@ -6,5 +6,92 @@ namespace Calculadora
 {
     class Multiplicacion
     {
+        /// <summary>
+        /// Multiplicacion de numeros
+        /// </summary>
+        /// <returns></returns>
+        public static decimal Multiply(decimal[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal multiplication = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                multiplication *= numbers[i];
+            }
+
+            return multiplication;
+        }
+
+        
+        /// <summary>
+        /// Multiplicacion de numeros pares
+        /// </summary>
+        /// <returns></returns>
+        public static decimal EvenNumMultiply(decimal[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal multiplication = 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0) {
+                    throw new ArgumentOutOfRangeException();
+                }
+                multiplication *= numbers[i];
+            }
+            return multiplication;
+        }
+
+       
+
+        /// <summary>
+        /// Multiplicacion donde el resultado no puede ser 0.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static decimal NotZeroMultiplication(decimal[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal multiplication = 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                multiplication *= numbers[i];
+            }
+            return multiplication;
+        }
+
     }
 }
