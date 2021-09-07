610191d9717e26c718516bf30c9dc2647e690577
Selene <selene.ajtd@gmail.com>
2021-09-07 19:22:01 +0200

    Carpeta services y pruebas metodos

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,8 +6,20 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-             
+            decimal[] numbers = { 3, 4, 5 };
+            decimal[] EvenNumbers = { 6, 8, 12 };
+            decimal[] NegativeNumbers = { -3, -4,-5 };
 
+            Console.WriteLine(Division.DivWithRest(4, 5));
+            Console.WriteLine(Division.PositiveDiv(6, 3));
+            Console.WriteLine(Suma.PositiveSum(numbers));
+            Console.WriteLine(Suma.Sum(numbers));
+            Console.WriteLine(Suma.NegativeSum(NegativeNumbers));
+            Console.WriteLine(Resta.SubtractionTwoNumbers(12,7));
+            Console.WriteLine(Resta.Subtraction(numbers));
+            Console.WriteLine(Multiplicacion.EvenNumMultiply(EvenNumbers));
+            Console.WriteLine(Multiplicacion.NotZeroMultiplication(numbers));
+            
         }
 
        
