7c64362725ade3cf1d3d955fa323d8498078eb04
Selene <selene.ajtd@gmail.com>
2021-09-07 16:17:57 +0200

    Suma

--- Calculadora/Suma.cs
+++ Calculadora/Suma.cs
@@ -28,5 +28,61 @@ namespace Calculadora
 
             return suma;
         }
+
+        public static decimal PositiveSum(decimal[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal suma = 0;
+
+            foreach (var number in numbers)
+            {
+ 
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                suma += number;
+
+            }
+            return suma;
+
+        }
+
+        public static decimal NegativeSum(decimal[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            decimal suma = 0;
+
+            foreach (var number in numbers)
+            {
+
+                if (number > 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                suma += number;
+
+            }
+            return suma;
+        }
+
     }
 }
