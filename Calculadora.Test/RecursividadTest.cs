dcfe7c787a2ed4de7544211e43b383bceefc9090
Selene <selene.ajtd@gmail.com>
2021-10-14 09:46:29 +0200

    Hate recursivity

---
+++ Calculadora.Test/RecursividadTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.Test
+{
+    [TestClass]
+    public class RecursividadTest
+    {
+        [TestMethod]
+        public void TestFactorial() {
+            var num = Recursividad.Factorial(5);
+            Assert.AreEqual(120, num);
+        }
+
+        [TestMethod]
+        public void TestFactorialFail()
+        {
+            var num = Recursividad.Factorial(5);
+            Assert.AreNotEqual(80, num);
+        }
+
+        [TestMethod]
+        public void TestFibonacci()
+        {
+            var num = Recursividad.Fibonacci(7);
+            Assert.AreEqual(13, num);
+        }
+
+
+        [TestMethod]
+        public void TestFibonacciFail()
+        {
+            var num = Recursividad.Fibonacci(7);
+            Assert.AreNotEqual(20, num);
+        }
+
+       
+
+    }
+}
---
+++ Calculadora/Recursividad.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public class Recursividad
+    {
+        public static int Factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n *= Factorial(n - 1);
+
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n;
+        }
+
+        public static int Fibonacci(int n)
+        {
+            // Fibonacci es la sucesion de : numero - 1 + numero -2
+            //0 = 0, 1 = 1
+
+            if (n == 0)
+            {
+                return 0;
+
+            }
+            else if (n == 1)
+            {
+
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+
+            }
+        }
+
+        public string Name = "paco";
+    
+       
+        //public static string ReverseString(string name)
+        //{
+        //    if(name.Length <= 1)
+        //    {
+        //        return name;
+        //    }
+        //    char LastChar = name[name.Length + 1];
+        //    string RestCahin = name.Substring(1);
+        //    string NewChain = "";
+        //}
+
+        //public static string ReverseString(string name, int index)
+        //{
+        //    char[] ArrayName = name.ToCharArray();
+
+        public static string HateRcursivity()
+        {
+            return "Odio la recursividad";
+        }
+
+
+
+        }
+    }
+}
